fbad205bcd6d7b3786250c94efae0ba16de1ba79
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2021-02-09 14:13:59 -0800

    Update initDatabase.sql

--- server/initDatabase.sql
+++ server/initDatabase.sql
@@ -1,9 +1,9 @@
 CREATE TABLE usercameras(
     id INTEGER PRIMARY KEY,
     ip VARCHAR(120),
-    password TEXT NOTNULL,
-    username TEXT NOTNULL,
-    last_time_login INTEGER NOTNULL
+    password TEXT NOT NULL,
+    username TEXT NOT NULL,
+    last_time_login INTEGER NOT NULL
 );
 CREATE TABLE userclients(
     cookie TEXT
