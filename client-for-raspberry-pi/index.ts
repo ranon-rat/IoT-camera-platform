74c6194f5dbacfa76bc4a8e09555446ee69eef96
ranon-rat <temporalsantiagoloco@gmail.com>
2021-03-30 21:53:10 -0600

    somestuff

--- client-for-raspberry-pi/index.ts
+++ client-for-raspberry-pi/index.ts
@@ -1,12 +1,26 @@
 import { StillCamera } from "pi-camera-connect";
+import axios, * from "axios"
+import * as fs from "fs";
+const host = "192.168.100.21:8080"
+const config:from.AxiosRequestConfig = {
+    method: "POST",
+    data: {
+        username: "penisCum",
+        password: "1234",
+    }
+};
 
-// Take still image and save to disk
-const runApp = async () => {
-  const stillCamera = new StillCamera();
+let token: string = "";// Take still image and save to disk
 
-  const image: Buffer = await stillCamera.takeImage();
+axios("http://" + host + "/register", config).then(r => r.data).catch();
+axios("http://" + host + "/login", config).then(r => token = r.data).catch();
+const runApp = async () =>
+{
+    const stillCamera = new StillCamera();
 
-  console.log(image.toString("base64"));
+    const image = await stillCamera.takeImage();
+
+    console.log(image.toString("base64"));
 };
 
 runApp();
