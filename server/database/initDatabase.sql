01651b07da297bf2a3d544109775da4399723ed0
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2021-04-06 18:28:46 -0700

    Update initDatabase.sql

--- server/database/initDatabase.sql
+++ server/database/initDatabase.sql
@@ -1,10 +1,10 @@
 -- if we need to add a new colum 
 -- ALTER TABLE <table> ADD <column> <type>;
 CREATE TABLE usercameras(
-    id          INTEGER,
-    ip          VARCHAR(64) PRIMARY KEY NOT NULL UNIQUE,
-    password    VARCHAR(64) NOT NULL UNIQUE,
-    token       VARCHAR(64) UNIQUE,
+    id          INTEGER PRIMARY KEY,
+    ip          VARCHAR(64)   NOT NULL UNIQUE,
+    password    VARCHAR(64)   NOT NULL UNIQUE,
+    token       VARCHAR(64)   UNIQUE,
     username    TEXT NOT NULL UNIQUE,
 
     last_time_login INTEGER NOT NULL   
